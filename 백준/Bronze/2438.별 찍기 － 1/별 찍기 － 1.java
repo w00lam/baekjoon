b30dccd3165a262b23e418776ad8b7bf9213953b
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-22 22:34:42 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 320 ms, Memory: 18080 KB -BaekjoonHub

---
+++ 백준/Bronze/2438.별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cnt = scanner.nextInt();
+
+        for (int i = 1; i <= cnt; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
