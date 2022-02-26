45c26821b71c8174aeed3bc92751bd01e867021a
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-26 21:53:21 +0900

    Update 평균.java

--- 백준/Bronze/1546.평균/평균.java
+++ 백준/Bronze/1546.평균/평균.java
@@ -2,23 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String args[]) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        double arr[] = new double[Integer.parseInt(bufferedReader.readLine())];
+        double arr[] = new double[Integer.parseInt(bufferedReader.readLine())]; // 테스트 케이스 개수 지정.
 
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) { // 테스트 케이스 생성.
             arr[i] = Double.parseDouble(stringTokenizer.nextToken());
         }
 
-        double sum = 0;
-        Arrays.sort(arr);
+        double sum = 0; // 수정한 총 시험점수의 합.
+        Arrays.sort(arr); // 각 테스트 케이스의 최대값을 구하기위해 정렬.
 
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i] / arr[arr.length - 1] * 100;
