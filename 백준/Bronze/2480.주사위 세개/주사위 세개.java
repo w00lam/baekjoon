b094f76cd926c66246e7312e035ac9b9643f4db8
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-26 21:34:32 +0900

    Update 주사위 세개.java

--- 백준/Bronze/2480.주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480.주사위 세개/주사위 세개.java
@@ -7,16 +7,13 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if(a == b && b == c) {
-            System.out.println(10000+(a*1000));
-        }
-        else if(a == b || a == c) {
-            System.out.println(1000+(a*100));
-        }
-        else if(b == c) {
-            System.out.println(1000+(b*100));
-        }
-        else {
+        if (a == b && b == c) {
+            System.out.println(10000 + (a * 1000));
+        } else if (a == b || a == c) {
+            System.out.println(1000 + (a * 100));
+        } else if (b == c) {
+            System.out.println(1000 + (b * 100));
+        } else {
             System.out.println((Math.max(a, Math.max(b, c)) * 100));
         }
     }
