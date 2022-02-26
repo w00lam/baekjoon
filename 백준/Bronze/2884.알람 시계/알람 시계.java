4f0c3b0cd479f576f095ef4db4763ced41bfc318
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-26 21:45:29 +0900

    Update 알람 시계.java

--- 백준/Bronze/2884.알람 시계/알람 시계.java
+++ 백준/Bronze/2884.알람 시계/알람 시계.java
@@ -7,16 +7,15 @@ public class Main {
         int H = scanner.nextInt();
         int M = scanner.nextInt();
 
-        if (M < 45) {
+        if (M < 45) { // 45분 빠르게 맞춰줘야함으로 입력값이 45분 보다 작으면 시간과 분을 따로 계산해준다.
             H--;
-            M = 60 - (45 - M);
+            M = 60 + M - 45;
             if (H < 0) {
                 H = 23;
             }
 
             System.out.println(H + " " + M);
-        }
-        else {
+        } else {
             System.out.println(H + " " + (M - 45));
         }
     }
