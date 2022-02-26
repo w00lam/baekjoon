809b927a34366de1ec12c5112e760b4ac611c57a
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-26 21:36:45 +0900

    Update 오븐 시계.java

--- 백준/Bronze/2525.오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525.오븐 시계/오븐 시계.java
@@ -7,14 +7,14 @@ public class Main {
         int m = sc.nextInt();
         int t = sc.nextInt();
 
-        h += t / 60;
-        m += t % 60;
+        h += t / 60; // 시 계산
+        m += t % 60; // 분 계산
 
-        if(m >= 60){
+        if (m >= 60) { // 분 -> 시
             h += 1;
             m -= 60;
         }
-        if(h >= 24){
+        if (h >= 24) { // 24시간 표기 수정
             h -= 24;
         }
 
