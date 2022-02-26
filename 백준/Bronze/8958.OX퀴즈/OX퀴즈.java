1c8bec7e7244d722da8250be67d8da53bc3768b4
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-26 21:51:01 +0900

    Update OX퀴즈.java

--- 백준/Bronze/8958.OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958.OX퀴즈/OX퀴즈.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
-        String arr[] = new String[scanner.nextInt()];
+        String arr[] = new String[scanner.nextInt()]; // 테스트 케이스 개수 지정.
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) { // 테스트 케이스 생성.
             arr[i] = scanner.next();
         }
 
         scanner.close();
 
         for (int i = 0; i < arr.length; i++) {
-            int cnt = 0;
-            int sum = 0;
+            int cnt = 0; // 연속되는 'O'의 개수
+            int sum = 0; // 총 점수
 
             for (int j = 0; j < arr[i].length(); j++) {
                 if (arr[i].charAt(j) == 'O') {
