b78fc0767c259124ed226cb4c834a8c948d37e61
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-26 21:29:55 +0900

    Update A＋B － 5.java

--- 백준/Bronze/10952.A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952.A＋B － 5/A＋B － 5.java
@@ -1,24 +1,23 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer;
+        StringTokenizer stringTokenizer; // 한 줄에 입력받은 문자열을 delim으로 분리하여 저장하면서 꺼내온다.
         StringBuilder stringBuilder = new StringBuilder();
 
-        while(true) {
-
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine()," ");
+        while (true) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
             int A = Integer.parseInt(stringTokenizer.nextToken());
             int B = Integer.parseInt(stringTokenizer.nextToken());
 
-            if(A==0 && B==0) {
+            if (A == 0 && B == 0) {
                 break;
             }
-            stringBuilder.append((A+B)).append('\n');
+            stringBuilder.append((A + B)).append('\n');
         }
         System.out.println(stringBuilder);
     }
