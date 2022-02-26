28809647da39250503dc3ed968c80380fe21e1bd
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-26 21:34:08 +0900

    Update 별 찍기 － 2.java

--- 백준/Bronze/2439.별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439.별 찍기 － 2/별 찍기 － 2.java
@@ -6,7 +6,7 @@ public class Main {
         int cnt = scanner.nextInt();
 
         for (int i = 1; i <= cnt; i++) {
-            for (int k = i; k < cnt; k++) {
+            for (int k = i; k < cnt; k++) { // 전체 줄(cnt) - 현재 줄(i)만큼 공백 출력 해주기.
                 System.out.printf(" ");
             }
             for (int j = 1; j <= i; j++) {
