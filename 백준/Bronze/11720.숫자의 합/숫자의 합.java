11d18f7e7dfbb4d949d904f8d5f7dda3f3e33aad
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-28 15:16:51 +0900

    Update 숫자의 합.java

--- 백준/Bronze/11720.숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720.숫자의 합/숫자의 합.java
@@ -13,5 +13,17 @@ public class Main {
         }
 
         System.out.println(sum);
+/**
+ BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ br.readLine();	// N 은 쓸모가 없으므로 입력만 받는다.
+
+ int sum = 0;
+
+ for(byte value : br.readLine().getBytes()) {
+ sum += (value - '0');	// 또는 (a-48)
+ }
+
+ System.out.print(sum);
+ */
     }
 }
