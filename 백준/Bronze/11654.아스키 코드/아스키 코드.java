1272cdda9fc7a0774d3c0bd535275549e85fe689
w00lam <98564214+w00lam@users.noreply.github.com>
2022-02-28 15:05:27 +0900

    Update 아스키 코드.java

--- 백준/Bronze/11654.아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654.아스키 코드/아스키 코드.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        int a = System.in.read();
+        int a = System.in.read(); // byte 값으로 문자 한 개만 읽으면서 해당 문자에 대응되는 아스키코드 값 저장.
         System.out.println(a);
     }
 }
