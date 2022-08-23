8d32a5b643da9225a28ca1fa8f8b3c5c1fc0e0ff
w00lam <98564214+w00lam@users.noreply.github.com>
2022-08-23 13:30:22 +0900

    [Silver V] Title: 소수, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(bufferedReader.readLine());
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int sum = 0;
+        int min = Integer.MIN_VALUE;
+
+        for (int i = M; i <= N; i++) {
+            boolean isPrime = true;
+            if (i == 1) {
+                continue;
+            }
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                sum += i;
+                if (min == Integer.MIN_VALUE) {
+                    min = i;
+                }
+            }
+        }
+
+        if(sum==0){
+            System.out.println(-1);
+        }else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
