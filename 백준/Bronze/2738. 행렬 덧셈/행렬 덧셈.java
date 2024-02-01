b12bd5335f16a196754d59fc5e4c62c68aa7766f
w00lam <98564214+w00lam@users.noreply.github.com>
2024-02-01 16:48:11 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 652 ms, Memory: 35776 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -17,7 +17,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                System.out.printf("%d ", matrix[i][j]);
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
