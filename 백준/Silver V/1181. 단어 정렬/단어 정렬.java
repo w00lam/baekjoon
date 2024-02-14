800781c35640e583108dfc03a559d123244f835c
w00lam <98564214+w00lam@users.noreply.github.com>
2024-02-14 17:58:12 +0900

    [Silver V] Title: 단어 정렬, Time: 348 ms, Memory: 27960 KB -BaekjoonHub

---
+++ 백준/Silver V/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            } else {
+                return o1.length() - o2.length();
+            }
+        });
+
+        sb.append(arr[0]).append('\n');
+        for (int i = 1; i < N; i++) {
+            if (!arr[i].equals(arr[i - 1])) {
+                sb.append(arr[i]).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
