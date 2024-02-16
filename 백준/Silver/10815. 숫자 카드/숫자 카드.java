13f5c41f63a02a85612cdf6e401903230be8418b
w00lam <98564214+w00lam@users.noreply.github.com>
2024-02-16 17:16:52 +0900

    [Silver V] Title: 숫자 카드, Time: 904 ms, Memory: 120564 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        HashMap<String, Integer> cardDeck = new HashMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cardDeck.put(st.nextToken(), 0);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            if (cardDeck.get(st.nextToken()) != null) {
+                sb.append('1').append(' ');
+            } else {
+                sb.append('0').append(' ');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
