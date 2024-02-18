05167cfbd682250550a4c2b312c4f087a1b9adfd
w00lam <98564214+w00lam@users.noreply.github.com>
2024-02-18 20:06:33 +0900

    [Silver IV] Title: 듣보잡, Time: 360 ms, Memory: 25980 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -5,20 +5,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        HashMap<String, Integer> noListen = new HashMap<>();
+        Set<String> noListen = new HashSet<>();
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < N; i++) {
             String name = br.readLine();
-            noListen.put(name, 0);
+            noListen.add(name);
         }
 
         List<String> nameList = new ArrayList<>();
         for (int i = 0; i < M; i++) {
             String name = br.readLine();
-            if (noListen.containsKey(name)) {
+            if (noListen.contains(name)) {
                 nameList.add(name);
             }
         }
