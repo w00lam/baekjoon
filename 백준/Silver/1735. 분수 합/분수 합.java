57a276c88a4424663e79220f8f5de539ab3c4c60
w00lam <98564214+w00lam@users.noreply.github.com>
2024-02-19 14:36:01 +0900

    [Silver III] Title: 분수 합, Time: 152 ms, Memory: 16004 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int numerator = a * d + b * c;
+        int denominator = b * d;
+
+        int mod = gcd(numerator, denominator);
+        numerator /= mod;
+        denominator /= mod;
+
+        bw.write(numerator + " " + denominator + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
