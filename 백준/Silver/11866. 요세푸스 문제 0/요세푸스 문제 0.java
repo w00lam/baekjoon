9784bb7929775cc7bb3452640077fd36eabc65bd
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-03-25 14:35:35 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 168 ms, Memory: 16408 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        List<Integer> removedNumbers = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                queue.offer(queue.poll());
+            }
+            removedNumbers.add(queue.poll());
+        }
+
+        String result = removedNumbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+
+        System.out.println("<" + result + ">");
+    }
+}
