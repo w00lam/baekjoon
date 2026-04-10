296273fe0c932ce3bd936f2b83a887278dd6de66
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-04-10 09:09:19 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.19 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] seoul) {
-        List<String> list = Arrays.asList(seoul);
-        int x = list.indexOf("Kim");
-        String answer = "김서방은 " + x + "에 있다";
-        return answer;
+        int x = Arrays.asList(seoul).indexOf("Kim");
+        return new StringBuilder("김서방은 ").append(x).append("에 있다").toString();
     }
 }
